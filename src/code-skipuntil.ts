8d45ecf3e6751a449670ad3c9681033a4b8fc046
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 23:40:50 +1100

    skiputil operator

---
+++ src/code-skipuntil.ts
@@ -0,0 +1,35 @@
+import {Observable} from 'rxjs/Rx';
+import {Subject} from 'rxjs/Rx';
+import {interval} from 'rxjs/Observable/interval';
+import 'rxjs/add/operator/skipUntil';
+
+
+var observable1 = Observable.create(
+    (data:any)=>{
+        var i=1
+        setInterval(
+            ()=>{
+                data.next(i++)
+            },1000
+        )
+    }
+    )
+
+    var observable2 = new Subject;
+
+    setTimeout(
+        ()=> {
+            observable2.next('Hey')
+        },3000
+        )
+
+    var newObs = observable1.skipUntil(observable2)
+
+    newObs.subscribe((x:any)=> addItem(x))
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
