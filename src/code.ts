b1f5ec493ea1d2b71624be0f3ec00f784f12c6ee
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 22:44:11 +1100

    hot observables

--- src/code.ts
+++ src/code.ts
@@ -2,3 +2,19 @@
 // fix for .share() is not a function
 import {Observable} from 'rxjs/Rx';
 import {fromEvent} from 'rxjs/Observable/fromEvent';
+
+//hot observables
+var observable = fromEvent(document,'mousemove');
+
+setTimeout(()=>{
+    var subscription = observable.subscribe(
+        (x:any)=>addItem(x)
+    )
+},2000);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
