ef7e0babb421558d98d004849c31e6f5ea816ea2
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 22:18:13 +1100

    cancelling subscription

--- src/code.ts
+++ src/code.ts
@@ -3,20 +3,30 @@ import { Observable } from "rxjs/Observable";
 // creating obbservable 
 var observable = Observable.create(
     (observer:any)=>{
-        observer.next('Hey guys!');
-        observer.next('how are you!');
-        observer.complete();
-        observer.next('This will not send');
+        try{
+        
+            observer.next('Hey guys!');
+            observer.next('how are you!');
+            setInterval(()=>{
+                observer.next('I am good!');
+            },2000)
+        }
+    catch(err){
+        observer.error(err);
     }
+}
+    
 );
 
-observable.subscribe(
+var observer = observable.subscribe(
     (x:any) => addItem(x),
     (error:any) => addItem(error),
     (complete:any) => addItem('Completed')
-    
-    
-    );
+);
+
+setTimeout(()=>{
+    observer.unsubscribe();
+},6001);
 
 function addItem(val:any) {
     var node = document.createElement("li");
