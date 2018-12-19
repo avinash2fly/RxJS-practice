6e6f9a9deda4f8724d2d30df334e6e648b181842
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 22:13:38 +1100

    observables basic

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,26 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+// creating obbservable 
+var observable = Observable.create(
+    (observer:any)=>{
+        observer.next('Hey guys!');
+        observer.next('how are you!');
+        observer.complete();
+        observer.next('This will not send');
+    }
+);
+
+observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    (complete:any) => addItem('Completed')
+    
+    
+    );
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
