e2f838632bdc381f25ca50ec4987a80364afb3b5
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 23:02:53 +1100

    replaySubject case 2

---
+++ src/code-replaySubject-basic.ts
@@ -0,0 +1,31 @@
+import {ReplaySubject} from 'rxjs/Rx';
+
+var subject = new ReplaySubject(2);
+
+subject.subscribe(
+    data => addItem('Observer 1:'+data),
+    err => addItem(err),
+    () => addItem('Observer 1 Completed')
+)
+
+subject.next('The first things has being set');
+subject.next('another things has being set');
+subject.next('...Observer 2 is about to subscribe');
+
+var observer2 = subject.subscribe(
+    data => addItem('Obbserver 2:' + data)
+)
+
+subject.next('the second thing has been sent');
+subject.next('the third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('Final thing has been set');
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
