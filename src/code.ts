5137dcebf9715cb965b41c1d8c8f6013afdd3e3c
Avinash Gupta <avinash2fly@gmail.com>
2018-12-19 23:40:28 +1100

    skiputil operator

--- src/code.ts
+++ src/code.ts
@@ -1,21 +1,31 @@
-import {from} from 'rxjs/Observable/from';
-import 'rxjs/add/operator/pluck';
+import {Observable} from 'rxjs/Rx';
+import {Subject} from 'rxjs/Rx';
+import {interval} from 'rxjs/Observable/interval';
+import 'rxjs/add/operator/skipUntil';
 
 
-from([
-    {
-        first:'Gary', last:'Simon', age:34
-    },
-    {
-        first:'Jane', last:'Simon', age:34
-    },
-    {
-        first:'John', last:'Simon', age:34
-    },
-])
+var observable1 = Observable.create(
+    (data:any)=>{
+        var i=1
+        setInterval(
+            ()=>{
+                data.next(i++)
+            },1000
+        )
+    }
+    )
 
-.pluck('first')
-.subscribe((x:any)=> addItem(x))
+    var observable2 = new Subject;
+
+    setTimeout(
+        ()=> {
+            observable2.next('Hey')
+        },3000
+        )
+
+    var newObs = observable1.skipUntil(observable2)
+
+    newObs.subscribe((x:any)=> addItem(x))
 
 function addItem(val:any) {
     var node = document.createElement("li");
